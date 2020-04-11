a728bbfa95604a4192d63d0e67bc8ab98fd60d93
天天向上 <john.yi@qq.com>
2020-04-11 22:33:36 +0800

    添加默认首页为index.html

--- his-prescription/src/main/java/cn/bdqn/his/prescription/config/MvcConfig.java
+++ his-prescription/src/main/java/cn/bdqn/his/prescription/config/MvcConfig.java
@@ -2,12 +2,18 @@ package cn.bdqn.his.prescription.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import cn.bdqn.his.common.interceptor.SsoCookieWrapperInterceptor;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addRedirectViewController("","index.html");
+	}
+
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new SsoCookieWrapperInterceptor())
